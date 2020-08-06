089b0ce837948cb31b96981b271c4028922bc9fb
Rajat kumar patel <61877737+prajat511@users.noreply.github.com>
2020-08-06 17:32:08 +0530

    Add files via upload

---
+++ faceRecog/urls.py
@@ -0,0 +1,34 @@
+"""faceRecog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from faceRecog import views as app_views
+from django.views.generic import TemplateView
+from django.urls import path
+
+urlpatterns = [
+    url(r'^$', app_views.index),
+    url(r'^error_image$', app_views.errorImg),
+    url(r'^create_dataset$', app_views.create_dataset),
+    url(r'^trainer$', app_views.trainer),
+   # url(r'^eigen_train$', app_views.eigenTrain),
+    url(r'^detect$', app_views.detect),
+   # url(r'^detect_image$', app_views.detectImage),
+    url(r'^admin/', admin.site.urls),
+    url(r'^records/', include('records.urls')),
+    path('sw.js',TemplateView.as_view(template_name='sw.js',content_type='application/javascript'),
+         name='sw.js',),
+]
